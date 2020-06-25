0db3a28239389450aff50417113975547a91d87a
treasureBear94 <qvo7896@gmail.com>
2020-06-26 01:39:10 +0900

    Initial commit

---
+++ src/test/java/com/treasurebear/KakaopayTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treasurebear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaopayTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
